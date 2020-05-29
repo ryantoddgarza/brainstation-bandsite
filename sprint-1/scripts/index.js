7baeb9ab995b1a2476bca5c7bbd74f2d57419e4a
ryantoddgarza <ryantoddgarza@gmail.com>
2020-05-28 23:34:00 -0700

    footer

---
+++ sprint-1/scripts/index.js
@@ -0,0 +1,6 @@
+// generate footer copyright year
+(function copyrightDate() {
+  let element = document.querySelector('#copyrightDate');
+  let year = new Date().getFullYear();
+  element.innerText = year
+})();
